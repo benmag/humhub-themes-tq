43b03bb8a55514501f339da4e7e21cdbca777b1b
Bec Todd <bec@bectodd.com>
2015-09-01 17:13:45 +1000

    TeachConnect - Home page
    Committing home page code & theme files

---
+++ theme design/views/layouts/head.php
@@ -0,0 +1,20 @@
+<?php $ver = HVersion::VERSION; ?>
+
+<!-- start: CSS -->
+<link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/datepicker.css?ver=<?php echo $ver; ?>" rel="stylesheet">
+<link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/theme.css?ver=<?php echo $ver; ?>" rel="stylesheet">
+<link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/flatelements.css?ver=<?php echo $ver; ?>" rel="stylesheet">
+<link href="<?php echo Yii::app()->theme->baseUrl; ?>/font/lato/lato.css" rel="stylesheet">
+<link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/theme-tc.css?ver=<?php echo $ver; ?>" rel="stylesheet">
+<link href="<?php echo Yii::app()->theme->baseUrl; ?>/js/owl-carousel/animate.css" rel="stylesheet">
+<link href="<?php echo Yii::app()->theme->baseUrl; ?>/js/owl-carousel/assets/owl.carousel.css" rel="stylesheet">
+<!-- end: CSS -->
+
+<!-- start: JavaScript -->
+ <script type="text/javascript"
+            src="<?php echo Yii::app()->theme->baseUrl; ?>/js/respond.js"></script> 
+ <script type="text/javascript"
+            src="<?php echo Yii::app()->theme->baseUrl; ?>/js/animatescroll/animatescroll.min.js"></script> 
+  <script type="text/javascript"
+            src="<?php echo Yii::app()->theme->baseUrl; ?>/js/owl-carousel/owl.carousel.min.js"></script> 
+<!-- end: JavaScript -->
