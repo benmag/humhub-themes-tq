bf5172cd16548c6dec9628ad876ae985808f7412
Bec Todd <bec@bectodd.com>
2015-09-03 21:12:14 +1000

    TeachConnect - Q&A screens design updates
     Q&A screens design updates

---
+++ theme design/views/questionanswer/widgets/profile.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This View shows a profile
+ *
+ * @property User $user is the user object
+ *
+ * @package application.modules.questionanswer
+ * @since 0.5
+ */
+?>
+
+<div class="media-body qanda-profile">
+
+	<a href="<?php echo $this->createUrl('//user/profile', array('uguid' => $user->guid)); ?>">
+		<span class="pull-left profile-size-sm">
+       		<img class="media-object img-rounded profile-size-sm"
+             src="<?php echo $user->getProfileImage()->getUrl(); ?>"
+             height="32" width="32" alt="32x32" data-src="holder.js/32x32"/>
+       		<div class="profile-overlay-img profile-overlay-img-sm"></div>
+		</span>
+	</a>
+        <div class="user-title pull-left hidden-xs">
+            <strong><?php echo CHtml::encode($user->displayName); ?> <?php echo "(".KarmaUser::model()->score($user->id).")"; ?></strong><br/><span class="truncate"><?php echo CHtml::encode($user->profile->title); ?></span>
+        </div>
+    
+</div>
