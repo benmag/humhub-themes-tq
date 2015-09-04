a8fe3865291e210161532d9f5b99aae9b9ec7a13
Bec Todd <bec@bectodd.com>
2015-09-04 16:19:21 +1000

    TeachConnect - Updated Short Banner for Q&A
    Updated Short Banner for Q&A

--- views/questionanswer/question/view.php
+++ views/questionanswer/question/view.php
@@ -4,14 +4,33 @@
 ?>
 
 <div class="container">
-    <div class="row">
-        <div class="col-md-9">
-        	<div class="row">
-            	<div class="col-xs-12">
-        			<div class="qa-circle-bg community-bg"><span class="icon icon-qanda"></span></div>
-        			<h1 class="qa-h1">Your Public Community Q&amp;A</h1>
+
+	<!-- Top Banner -->
+    <div class="row" style="margin-bottom:40px;">
+        <div class="col-md-12">
+            <div class="panel panel-default panel-profile">
+                <div class="panel-profile-header">
+                    <div class="image-upload-container" style="width: 100%; height: 100%; overflow:hidden;">
+                        <img class="img-profile-header-background img-profile-header-background-qanda" id="space-banner-image" src="<?php echo Yii::app()->theme->baseUrl; ?>/img/tc-qanda-banner.png" style="width: 100%;" width="100%">
+            
+                        <div class="img-profile-data">
+                            <h1 class="space">Community Knowledge Q&amp;A</h1>
+                            <h2 class="space">A searchable repository of teaching knowledge.</h2>
+                        </div>
+                    </div>
+    
+                    <div class="image-upload-container profile-user-photo-container" style="width: 140px; height: 140px;">
+                        <img class="img-rounded profile-user-photo" id="space-profile-image" src="<?php echo Yii::app()->theme->baseUrl; ?>/img/tc-profile-qanda.png" data-src="holder.js/140x140" alt="140x140" style="width: 140px; height: 140px;">
+                    </div>
+    
+    
                 </div>
             </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-9">
             <div class="panel panel-default qanda-panel qanda-panel-padding">
                 <div class="panel-body">
                     <div class="media">
