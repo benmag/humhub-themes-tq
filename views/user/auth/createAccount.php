0384a90c6ff3b4fc91f923af4fc3b98336925a20
QUT <benmag@users.noreply.github.com>
2015-09-08 11:09:14 +1000

    use anon account module

---
+++ views/user/auth/createAccount.php
@@ -0,0 +1,14 @@
+<?php
+if(isset(Yii::app()->modules['anon_saccounts'])) {
+
+	Yii::app()->request->redirect(Yii::app()->createUrl('//anon_accounts/main/index', array('token' => $_GET['token'])));
+
+} else {
+
+	$controller = Yii::app()->controller;
+	$controller->renderPartial('application.modules.user.views.auth.createAccount_original', array(
+        'form' => $form,
+        'needAproval' => false)
+    );
+}
+?>
