3efffcaf2be1018bb9b108e4b4bea8efdb0dbd23
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-01-07 13:08:07 +1000

    Removed non-existent $ver
    New HumHub core no longer uses $ver

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -1,10 +1,10 @@
 <!-- start: CSS -->
-<link href="<?php echo $this->theme->getBaseUrl(); ?>/css/datepicker.css?ver=<?php echo $ver; ?>" rel="stylesheet">
-<link href="<?php echo $this->theme->getBaseUrl(); ?>/css/theme.css?ver=<?php echo $ver; ?>" rel="stylesheet">
-<link href="<?php echo $this->theme->getBaseUrl(); ?>/css/flatelements.css?ver=<?php echo $ver; ?>" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl(); ?>/css/datepicker.css" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl(); ?>/css/theme.css" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl(); ?>/css/flatelements.css" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl(); ?>/font/lato/lato.css" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl(); ?>/font/openionic/style.css" rel="stylesheet">
-<link href="<?php echo $this->theme->getBaseUrl(); ?>/css/theme-tc.css?ver=<?php echo $ver; ?>" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl(); ?>/css/theme-tc.css" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl(); ?>/js/owl-carousel/animate.css" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl(); ?>/js/owl-carousel/assets/owl.carousel.css" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl(); ?>/js/scrollbar/jquery.mCustomScrollbar.min.css" rel="stylesheet">
