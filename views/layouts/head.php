50223b3175764d96ce0f0aae2d8701661988f80c
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-01-07 12:53:41 +1000

    Now follows migration guide
    Head now follows migration guide, as per docs.
    https://www.humhub.org/docs/guide-theming-migrate.html

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -1,29 +1,27 @@
-<?php $ver = HVersion::VERSION; ?>
-
 <!-- start: CSS -->
-<link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/datepicker.css?ver=<?php echo $ver; ?>" rel="stylesheet">
-<link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/theme.css?ver=<?php echo $ver; ?>" rel="stylesheet">
-<link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/flatelements.css?ver=<?php echo $ver; ?>" rel="stylesheet">
-<link href="<?php echo Yii::app()->theme->baseUrl; ?>/font/lato/lato.css" rel="stylesheet">
-<link href="<?php echo Yii::app()->theme->baseUrl; ?>/font/openionic/style.css" rel="stylesheet">
-<link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/theme-tc.css?ver=<?php echo $ver; ?>" rel="stylesheet">
-<link href="<?php echo Yii::app()->theme->baseUrl; ?>/js/owl-carousel/animate.css" rel="stylesheet">
-<link href="<?php echo Yii::app()->theme->baseUrl; ?>/js/owl-carousel/assets/owl.carousel.css" rel="stylesheet">
-<link href="<?php echo Yii::app()->theme->baseUrl; ?>/js/scrollbar/jquery.mCustomScrollbar.min.css" rel="stylesheet">
-<link href="<?php echo Yii::app()->theme->baseUrl; ?>/js/bootstrap-select/dist/css/bootstrap-select.css" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl(); ?>/css/datepicker.css?ver=<?php echo $ver; ?>" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl(); ?>/css/theme.css?ver=<?php echo $ver; ?>" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl(); ?>/css/flatelements.css?ver=<?php echo $ver; ?>" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl(); ?>/font/lato/lato.css" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl(); ?>/font/openionic/style.css" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl(); ?>/css/theme-tc.css?ver=<?php echo $ver; ?>" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl(); ?>/js/owl-carousel/animate.css" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl(); ?>/js/owl-carousel/assets/owl.carousel.css" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl(); ?>/js/scrollbar/jquery.mCustomScrollbar.min.css" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl(); ?>/js/bootstrap-select/dist/css/bootstrap-select.css" rel="stylesheet">
 <!-- end: CSS -->
 
 <!-- start: JavaScript -->
  <script type="text/javascript"
-            src="<?php echo Yii::app()->theme->baseUrl; ?>/js/respond.js"></script> 
+            src="<?php echo $this->theme->getBaseUrl(); ?>/js/respond.js"></script>
  <script type="text/javascript"
-            src="<?php echo Yii::app()->theme->baseUrl; ?>/js/animatescroll/animatescroll.min.js"></script> 
+            src="<?php echo $this->theme->getBaseUrl(); ?>/js/animatescroll/animatescroll.min.js"></script>
  <script type="text/javascript"
-            src="<?php echo Yii::app()->theme->baseUrl; ?>/js/owl-carousel/owl.carousel.js"></script> 
+            src="<?php echo $this->theme->getBaseUrl(); ?>/js/owl-carousel/owl.carousel.js"></script>
  <script type="text/javascript"
-            src="<?php echo Yii::app()->theme->baseUrl; ?>/js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+            src="<?php echo $this->theme->getBaseUrl(); ?>/js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
  <script type="text/javascript"
-            src="<?php echo Yii::app()->theme->baseUrl; ?>/js/bootstrap-select/dist/js/bootstrap-select.js"></script>
+            src="<?php echo $this->theme->getBaseUrl(); ?>/js/bootstrap-select/dist/js/bootstrap-select.js"></script>
             
 <script type="text/javascript">
 	// Initiate Bootstrap Tooltips
