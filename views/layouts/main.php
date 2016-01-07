3b7255d41416e393f1feaccf446834b0d0444016
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-01-07 12:41:47 +1000

    Removed deprecated code snippet
    HVersion no longer required as per
    https://www.humhub.org/docs/guide-theming-migrate.html

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -11,8 +11,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <!-- end: Mobile Specific -->
 
-    <?php $ver = HVersion::VERSION; ?>
-
     <!-- start: CSS -->
     <link href="<?php echo Yii::app()->baseUrl; ?>/css/animate.min.css?ver=<?php echo $ver; ?>" rel="stylesheet">
     <link href="<?php echo Yii::app()->baseUrl; ?>/css/bootstrap.min.css?ver=<?php echo $ver; ?>" rel="stylesheet">
