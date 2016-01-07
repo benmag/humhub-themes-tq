17ce517b1ff6d1363db7aaf1615465f2261d6c7d
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-01-07 16:48:54 +1000

    Migrated to New
    accountTopMenu now should be accepted by the new HumHub.

--- views/user/widgets/accountTopMenu.php
+++ views/user/widgets/accountTopMenu.php
@@ -1,4 +1,11 @@
-<?php if (Yii::app()->user->isGuest): ?>
+<?php
+use yii\helpers\Html;
+use yii\helpers\Url;
+use humhub\models\Setting;
+$user = Yii::$app->user->getIdentity();
+?>
+
+<?php if (Yii::$app->user->isGuest): ?>
 
     <a href="<?php echo $this->createUrl('//user/auth/login'); ?>" class="btn btn-enter" data-toggle="modal" data-target="#globalModal">Sign in / up</a>
 
@@ -8,12 +15,12 @@
         <li class="dropdown account">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                 <div class="user-title pull-left hidden-xs">
-                    <strong><?php echo CHtml::encode(Yii::app()->user->displayName); ?></strong><br/><span class="truncate"><?php echo CHtml::encode(Yii::app()->user->getModel()->profile->title); ?></span>
+                    <strong><?php echo Html::encode( $user->getDisplayName() ); ?></strong><br/><span class="truncate"><?php echo Html::encode($user->profile->title); ?></span>
                 </div>
 				
     			<div class="profile-size-sm profile-img-navbar">
                     <img id="user-account-image profile-size-sm" class="img-rounded"
-                         src="<?php echo Yii::app()->user->model->getProfileImage()->getUrl(); ?>"
+                         src="<?php echo $user->getProfileImage()->getUrl(); ?>"
                          height="32" width="32" alt="32x32" data-src="holder.js/32x32"/>
                     <div class="profile-overlay-img profile-overlay-img-sm"></div>
                 </div>
@@ -22,20 +29,20 @@
             </a>
             <ul class="dropdown-menu pull-right">
                 <li>
-                    <a href="<?php echo $this->createUrl('//user/profile', array('uguid' => Yii::app()->user->guid)); ?>"><i
+                    <a href="<?php echo URL::to(['user/profile', 'uguid' => Yii::$app->user->getId()]); ?>"><i
                             class="fa fa-user"></i> <?php echo Yii::t('base', 'My profile'); ?>
                     </a>
                 </li>
                 <li>
-                    <a href="<?php echo $this->createUrl('//user/account/edit') ?>"><i
+                    <a href="<?php echo URL::to(['user/account/edit']); ?>"><i
                             class="fa fa-edit"></i> <?php echo Yii::t('base', 'Account settings'); ?>
                     </a>
                 </li>
 
-                <?php if (Yii::app()->user->isAdmin()) : ?>
+                <?php if (Yii::$app->user->isAdmin()) : ?>
                     <li class="divider"></li>
                     <li>
-                        <a href="<?php echo $this->createUrl('//admin/index') ?>"><i
+                        <a href="<?php echo URL::to(['//admin/index']); ?>"><i
                                 class="fa fa-cogs"></i> <?php echo Yii::t('base', 'Administration'); ?>
                         </a>
                     </li>
@@ -43,7 +50,7 @@
 
 
                 <!-- if the current user has admin rights -->
-                <?php if (HSetting::Get('needApproval', 'authentication_internal') && (Yii::app()->user->isAdmin() || Yii::app()->user->canApproveUsers())) : ?>
+                <?php if (humhub\models\Setting::Get('needApproval', 'authentication_internal') && (Yii::$app->user->isAdmin() || Yii::$app->user->canApproveUsers())) : ?>
                     <li>
                         <a href="<?php echo $this->createUrl('//admin/approval') ?>"><i
                                 class="fa fa-check-circle"></i> <?php echo Yii::t('base', 'User Approvals'); ?>
@@ -52,10 +59,10 @@
                 <?php endif; ?>
 
 
-                <?php if (!isset(Yii::app()->session['ntlm']) || Yii::app()->session['ntlm'] == false) : ?>
+                <?php if (!isset(Yii::$app->session['ntlm']) || Yii::$app->session['ntlm'] == false) : ?>
                     <li class="divider"></li>
                     <li>
-                        <a href="<?php echo $this->createUrl('//user/auth/logout') ?>"><i
+                        <a href="<?php echo URL::to('/user/auth/logout') ?>"><i
                                 class="fa fa-sign-out"></i> <?php echo Yii::t('base', 'Logout'); ?>
                         </a>
                     </li>
