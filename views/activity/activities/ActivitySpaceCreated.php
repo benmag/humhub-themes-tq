7be11c58b9e9b82ca8753615ee8f43c71a4e63c7
Den Volkov <volkovdenya@gmail.com>
2016-06-16 17:27:45 +0300

    TC-10 fixed color spaces

--- views/activity/activities/ActivitySpaceCreated.php
+++ views/activity/activities/ActivitySpaceCreated.php
@@ -1,7 +1,10 @@
 <?php $this->beginContent('application.modules_core.activity.views.activityLayout', array('activity' => $activity)); ?>
 
 <?php if ($workspace != null && !Yii::app()->controller instanceof ContentContainerController): ?>
-    <?php echo Yii::t('ActivityModule.views_activities_ActivitySpaceCreated', "<i class='fa fa-dot-circle-o color-circle-mentorship' style='margin-right: 5px;vertical-align: middle;'></i> %displayName% created the new space %spaceName%", array(
+    <?php
+    $color = ($workspace->name == 'Welcome Space')?'color:red !important':'color:blue !important';
+    ?>
+    <?php echo Yii::t('ActivityModule.views_activities_ActivitySpaceCreated', "<i class='fa fa-dot-circle-o color-circle-mentorship' style='margin-right: 5px;vertical-align: middle;$color '></i> %displayName% created the new space %spaceName%", array(
         '%displayName%' => '<strong>'.CHtml::encode($user->displayName).'</strong>',
         '%spaceName%' => '<strong>'. CHtml::encode(Helpers::truncateText($workspace->name, 25)).'</strong>'
     )); ?>
