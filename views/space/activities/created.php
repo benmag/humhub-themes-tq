0078989bd4ec8310a1858d2b4bfa0f8ee54979c7
VolkovDenis <volkovdenya@gmail.com>
2016-09-06 11:44:31 +0300

    fixed var

--- views/space/activities/created.php
+++ views/space/activities/created.php
@@ -4,6 +4,8 @@ use yii\helpers\Html;
 use humhub\libs\Helpers;
 use humhub\modules\content\components\ContentContainerController;
 
+$class_color = ($workspace->name == 'Welcome Space')?'color-circle-welcome':'color-circle-mentorship';
+
 if (!Yii::$app->controller instanceof ContentContainerController) {
     echo Yii::t('ActivityModule.views_activities_ActivitySpaceCreated',"<i class='fa fa-dot-circle-o $class_color' style='margin-right: 5px;vertical-align: middle;'></i> " .  "%displayName% created the new space %spaceName%", array(
         '%displayName%' => '<strong>' . Html::encode($originator->displayName) . '</strong>',
