b23dae68bac79d8de0cb8076b551f66c6ce308a4
VolkovDenis <volkovdenya@gmail.com>
2016-09-06 11:15:32 +0300

    TC-134 add icon to space

--- views/space/activities/created.php
+++ views/space/activities/created.php
@@ -5,12 +5,12 @@ use humhub\libs\Helpers;
 use humhub\modules\content\components\ContentContainerController;
 
 if (!Yii::$app->controller instanceof ContentContainerController) {
-    echo Yii::t('ActivityModule.views_activities_ActivitySpaceCreated', "%displayName% @@@33333created the new space %spaceName%", array(
+    echo Yii::t('ActivityModule.views_activities_ActivitySpaceCreated',"<i class='fa fa-dot-circle-o $class_color' style='margin-right: 5px;vertical-align: middle;'></i> " .  "%displayName% created the new space %spaceName%", array(
         '%displayName%' => '<strong>' . Html::encode($originator->displayName) . '</strong>',
         '%spaceName%' => '<strong>' . Html::encode(Helpers::truncateText($source->name, 25)) . '</strong>'
     ));
 } else {
-    echo Yii::t('ActivityModule.views_activities_ActivitySpaceCreated', "%displayName% @@@3333created this space.", array(
+    echo Yii::t('ActivityModule.views_activities_ActivitySpaceCreated',"<i class='fa fa-dot-circle-o $class_color' style='margin-right: 5px;vertical-align: middle;'></i> " .  "%displayName% created this space.", array(
         '%displayName%' => '<strong>' . Html::encode($originator->displayName) . '</strong>'
     ));
 }
